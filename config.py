4812fdca2dcc9fd9d892ed07a39e8a4abfea4b19
jhiimr <152472353+jhiimr@users.noreply.github.com>
2023-11-30 15:23:32 +0900

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,3 @@ PAD = 5
 
 ### Seperator
 PARSER = chr(0)
-
-### test data
-TEST_DATA = ['원시데이터 (Raw Data)\x00기계학습올 목적으로\x00권특 단계메서 수집 또는 생싱한 음성 이미지 엉상, 텍스트 등의 데이터\x00현전데미터 (Sourco Data\x00Unlabeled Data}\x00원시데이터들 라빌림 공정 투입하기 위하 필요한 전저리 둥 정제 작업올 수행한 데이터로\x00라엘럽데이터가 부여되지 않은 상태의 데기터\x00인공지능 (Artificial Intelligance)\x00가C 어머이이드', "원시데이터 (Raw Data) 기계학습을 목적으로 화독 단계에서 수집 또는 생성한 음성, 이미지, 영상, 텍스트 등의 데어터 원천데이터 (Source Data, Unlabeled Data) 웹시펙터월 적별링 긍정에 투입하기 위해 필요한 전차의 등 정제 직업을 수별한 데이터로르' 라벨링데이터가 부여되지 않은 상태의 테기터 인공지능 (Artificial Intelligence) 자여 어서이 시케"]
